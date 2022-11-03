77c9a8fff59b5765ca692d2577ee9c43e132e578
Gabriel Stenzowski <gabriel.stenzowskicav@gmail.com>
2022-11-03 16:43:57 -0300

    Versão 0.3
    Adicionado JavaScript, através de funções

---
+++ js/script.js
@@ -0,0 +1,50 @@
+function createGame(time1, hora, time2) {
+  return `   
+  <li>
+    <img src="assets/icon-${time1}.svg" alt="Bandeira ${time1}" />
+    <strong>${hora}</strong>
+    <img src="assets/icon-${time2}.svg" alt="Bandeira ${time2}" />
+  </li> 
+  `
+}
+
+function createCard(data, dia, jogos) {
+  return `
+  <div class="card">
+          <h2>${data} <span>${dia} </span></h2>
+          <ul>
+            ${jogos}
+          </ul>  
+  </div> 
+  `
+}
+document.querySelector("#app").innerHTML = `
+      <header>
+        <img src="assets/logo.svg" alt="Logo da NLW" />
+      </header>
+      <main id="cards">
+        ${createCard(
+          "20/11",
+          "domingo",
+          createGame("qatar", "13:00", "ecuador")
+        )}
+        ${createCard(
+          "24/11",
+          "quinta",
+          createGame("switzerland", "07:00", "cameroon") +
+            createGame("uruguay", "10:00", "southKorea") +
+            createGame("portugal", "13:00", "ghana") +
+            createGame("brazil", "16:00", "serbia")
+        )}
+        ${createCard(
+          "28/11",
+          "segunda",
+          createGame("brazil", "13:00", "switzerland")
+        )}
+        ${createCard(
+          "02/12",
+          "sexta",
+          createGame("cameroon", "16:00", "brazil")
+        )}
+      </main>
+`
