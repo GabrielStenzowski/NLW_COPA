6bc9f699f0c5e1e5f38185dd0cf4e65dddd834f6
Gabriel Stenzowski <gabriel.stenzowskicav@gmail.com>
2022-11-06 19:22:09 -0300

    Versão 1.0
    projeto final

--- js/script.js
+++ js/script.js
@@ -18,33 +18,15 @@ function createCard(data, dia, jogos) {
   </div> 
   `
 }
-document.querySelector("#app").innerHTML = `
-      <header>
-        <img src="assets/logo.svg" alt="Logo da NLW" />
-      </header>
-      <main id="cards">
-        ${createCard(
-          "20/11",
-          "domingo",
-          createGame("qatar", "13:00", "ecuador")
-        )}
-        ${createCard(
-          "24/11",
-          "quinta",
-          createGame("switzerland", "07:00", "cameroon") +
-            createGame("uruguay", "10:00", "southKorea") +
-            createGame("portugal", "13:00", "ghana") +
-            createGame("brazil", "16:00", "serbia")
-        )}
-        ${createCard(
-          "28/11",
-          "segunda",
-          createGame("brazil", "13:00", "switzerland")
-        )}
-        ${createCard(
-          "02/12",
-          "sexta",
-          createGame("cameroon", "16:00", "brazil")
-        )}
-      </main>
-`
+document.querySelector("#cards").innerHTML =
+  createCard("20/11", "domingo", createGame("qatar", "13:00", "ecuador")) +
+  createCard(
+    "24/11",
+    "quinta",
+    createGame("switzerland", "07:00", "cameroon") +
+      createGame("uruguay", "10:00", "southKorea") +
+      createGame("portugal", "13:00", "ghana") +
+      createGame("brazil", "16:00", "serbia")
+  ) +
+  createCard("28/11", "segunda", createGame("brazil", "13:00", "switzerland")) +
+  createCard("02/12", "sexta", createGame("cameroon", "16:00", "brazil"))
